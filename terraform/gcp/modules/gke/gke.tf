4f95f8a45504093c0c2455a509c65b8818ff9193
Randy Cahyana <randy@rndir.cc>
2021-07-16 08:43:01 +0700

    gcp: update gke node-poolscopes
    This add missing scopes for compute instance to pull images from
    container registry and google cloud endpoints.

--- terraform/gcp/modules/gke/gke.tf
+++ terraform/gcp/modules/gke/gke.tf
@@ -43,6 +43,9 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
     ]
 
     labels = {
